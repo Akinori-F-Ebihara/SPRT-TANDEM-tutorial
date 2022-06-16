2e6a59aa167227de7602146d1d91c9810de7a738
Akinori-F-Ebihara <61033362+Akinori-F-Ebihara@users.noreply.github.com>
2022-06-16 11:15:15 +0900

    Update make_nmnist.py

--- make_nmnist.py
+++ make_nmnist.py
@@ -21,18 +21,13 @@ import tensorflow_datasets as tfds
 ############## USER DEFINED #####################
 data_dir = "./data-directory/tensorflow_datasets" 
     # MNIST will be downloaded to this directory
-train_or_test = "train" #"test"
-    # "train" (train dataset will be made) or "test" (test dataset will be made). 
-    # Make sure to execute this script TWICE. 
-    # One is to make the training dataset; the othrer is to make the test dataset.
-record_file = "./data-directory/nosaic_mnist_{}.tfrecords".format(train_or_test)
+phase = ["train", "test"]
+record_file_prefix = "./data-directory/nosaic_mnist"
     # TFR name
 #################################################
 
 # Functions
 def set_gpu_devices(gpu):
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
     tf.config.experimental.set_visible_devices(physical_devices[gpu], 'GPU')
     tf.config.experimental.set_memory_growth(physical_devices[gpu], True)
 
@@ -68,8 +63,12 @@ def fix_random_seed(flag_seed, seed=None):
     else:
         print("Random seed not fixed.")
     
-
-set_gpu_devices(0) # the first GPU (numbered 0) will be used
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    print('Found GPU(s) for acceleration.')
+    set_gpu_devices(0) # the first GPU (numbered 0) will be used
+else:
+    print("Not enough GPU hardware devices available, running on CPUs...")
 fix_random_seed(True, 7)
 
 # Load MNIST as numpy array
@@ -80,65 +79,69 @@ images_test = dsts["image"].numpy()  # (60000,), np.int64
 labels_train = dstr["label"].numpy() # (10000, 28, 28, 1), np.uint8
 labels_test = dsts["label"].numpy()  # (10000,), np.int64
 
-# train or test
-if train_or_test == "train":
-    images_make = images_train
-    labels_make = labels_train
-elif train_or_test == "test":
-    images_make = images_test
-    labels_make = labels_test
-else:
-    raise ValueError(train_or_test)
+for train_or_test in phase:
+    print('start working on {} phase.'.format(train_or_test))
+    # train or test
+    if train_or_test == "train":
+        images_make = images_train
+        labels_make = labels_train
+    elif train_or_test == "test":
+        images_make = images_test
+        labels_make = labels_test
+    else:
+        raise ValueError(train_or_test)
 
-# Make training "OR" test data of nosaic MNIST
-############################################################
-if os.path.exists(record_file):
-    raise ValueError("record_file exists. Remove or rename it.")
-    
-with tf.io.TFRecordWriter(record_file) as writer:
-    cnt = 1
-    for image, label in zip(images_make, labels_make):
-        # Verbose
-        if cnt % 1000 == 0:
-            print("Iteration {}/{}".format(cnt, len(images_make)))
-        cnt += 1
+    record_file = record_file_prefix + "_{}.tfrecords".format(train_or_test)
 
-        # Reshape    
-        image_reshape = np.reshape(image, (28, 28))
+    # Make training "OR" test data of nosaic MNIST
+    ############################################################
+    if os.path.exists(record_file):
+        raise ValueError("record_file exists. Remove or rename it.")
+        
+    with tf.io.TFRecordWriter(record_file) as writer:
+        cnt = 1
+        for image, label in zip(images_make, labels_make):
+            # Verbose
+            if cnt % 1000 == 0:
+                print("Iteration {}/{}".format(cnt, len(images_make)))
+            cnt += 1
 
-        # 1. Generate a sequence of offsets of nosaic
-        ########################################
-        # Remove 40 or 28 noisy pixels in each step (40 * 19 + 28 = 784(= 28*28*1))
-        idx_perm = np.random.permutation(784) 
-        idx_perm_split = [idx_perm[40*k: 40*k+40] for k in range(0, 20)] # (20, 28 if last cell (offset=19) else 40 )
+            # Reshape    
+            image_reshape = np.reshape(image, (28, 28))
 
-        # 2. Make masks that are filled with 255
-        ########################################
-        masks = [None]*20
-        bkgd = np.array([255]*784, dtype=np.uint32)
-        for i, offsets in enumerate(idx_perm_split):
-            if i == 0:
-                masks[i] = bkgd - 0
-            else:
-                masks[i] = masks[i-1] - 0 
-            masks[i][offsets] = masks[i][offsets] - 255
+            # 1. Generate a sequence of offsets of nosaic
+            ########################################
+            # Remove 40 or 28 noisy pixels in each step (40 * 19 + 28 = 784(= 28*28*1))
+            idx_perm = np.random.permutation(784) 
+            idx_perm_split = [idx_perm[40*k: 40*k+40] for k in range(0, 20)] # (20, 28 if last cell (offset=19) else 40 )
 
-        # Reshape
-        for i, mask in enumerate(masks):
-            masks[i] = np.reshape(mask, (28,28))
+            # 2. Make masks that are filled with 255
+            ########################################
+            masks = [None]*20
+            bkgd = np.array([255]*784, dtype=np.uint32)
+            for i, offsets in enumerate(idx_perm_split):
+                if i == 0:
+                    masks[i] = bkgd - 0
+                else:
+                    masks[i] = masks[i-1] - 0 
+                masks[i][offsets] = masks[i][offsets] - 255
 
-        # 3. Synthesize the mask and the image
-        ########################################
-        masked_images = [None]*20
-        image_org = np.uint32(image_reshape)
+            # Reshape
+            for i, mask in enumerate(masks):
+                masks[i] = np.reshape(mask, (28,28))
 
-        for i, mask in enumerate(masks):
-            masked_images[i] = np.uint8(np.clip(image_org + mask, 0, 255))
+            # 3. Synthesize the mask and the image
+            ########################################
+            masked_images = [None]*20
+            image_org = np.uint32(image_reshape)
 
-        # Reshape
-        video = np.reshape(masked_images, (20, 28, 28, 1)) # from (20,28,28) to (20,28,28,1)
+            for i, mask in enumerate(masks):
+                masked_images[i] = np.uint8(np.clip(image_org + mask, 0, 255))
 
-        # 4. Save images
-        ########################################
-        label = np.int64(label) 
-        np_to_tfr_nosaic_mnist(x=video, y=label, writer=writer)
+            # Reshape
+            video = np.reshape(masked_images, (20, 28, 28, 1)) # from (20,28,28) to (20,28,28,1)
+
+            # 4. Save images
+            ########################################
+            label = np.int64(label) 
+            np_to_tfr_nosaic_mnist(x=video, y=label, writer=writer)
