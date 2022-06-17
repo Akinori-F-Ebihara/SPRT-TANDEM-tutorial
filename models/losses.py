735a46fa6272dd0b5254347e51ed5b1d134311cb
Akinori-F-Ebihara <61033362+Akinori-F-Ebihara@users.noreply.github.com>
2022-06-17 13:36:17 +0900

    misc.updates

--- models/losses.py
+++ models/losses.py
@@ -338,6 +338,215 @@ def LLLR(logits_concat, labels_concat, oblivious, version, flag_mgn,
     return lllr # scalar
 
 
+def LLLR_ver2(logits_concat, labels_concat, oblivious, version, flag_mgn, 
+    flag_prefactor=None, flag_prior_ratio=None, classwise_sample_sizes=None, beta=None):
+    """ LLLR for early multi-classification of time series.
+        Cost-sensitive loss weighting is supported,
+    Args:
+        logits_concat: A logit Tensor with shape
+            (batch, (duration - order_sprt), order_sprt + 1, num classes). 
+            This is the output from 
+            datasets.data_processing.sequential_concat(logit_slice, labels_slice)
+        labels_concat: A label Tensor with shape (batch size,). 
+            This is the output from 
+            datasets.data_processing.sequential_concat(logit_slice, labels_slice).
+        oblivious: A bool, whether to use TANDEMwO or not (= TANDEM).
+        version: "A", "B", "C", "D", "E", or "F".
+        flag_mgn: Use margin or not.
+        flag_prior_ratio: A bool. Subtract the prior ratio term to the LLR or not.
+        flag_prefactor: A bool. Add the prior ratio term to the LLR or not.
+            Class-balanced-loss-like prior ratios supported.
+        classwise_sample_sizes: A list of integers. 
+            The length is equal to the number of classes.
+        beta: A float larger than 0. Larger beta leads to 
+            more discriminative weights.
+            If beta is None, normal, non-cost-sensitive learning
+            will be done. If beta = 1, then weights are simply 
+            the inverse class frequencies (1 / N_k, 
+            where N_k is the sample size of class k).
+            If beta = -1, weights = [1,1,...,1] (len=num classes).
+            This is useful for non-cost-sensitive learning.
+    Return:
+        llr_loss: A scalar Tensor that represents the log-likelihoood ratio loss.
+    Remark:
+        - version A: original (use all LLRs)
+        - version B: simple LLLR (extract the positive class' raw)
+        - version C: logistic LLLR (logistic loss instead of sigmoid)
+        - version D: simple logistic LLLR (logitstic loss, extract the positive class' raw)
+        - version E: LSEL (log-sum-exp loss)
+        - version F: modLSEL
+        - margin is generated uniformly-randomly.
+    An example of assert checks for operation confirmation: 
+        dummy = classwise_sample_sizes
+        for _ in range(10):
+            batch_size = 16
+            duration = 50
+            order_sprt = 20
+            num_classes = 101
+            beta = 1 # - 1e-2 # 1e-2 to 1e-5 or 1e-7
+            logits = np.float32(np.random.rand(batch_size, duration, num_classes))
+            labels = np.int32(np.random.randint(0, num_classes - 1, [batch_size]))
+            logits_slice, labels_slice = sequential_slice(logits, labels, order_sprt)
+            logits_concat, labels_concat = sequential_concat(logits_slice, labels_slice, duration)
+
+            lllr1 = LLLR(logits_concat, labels_concat, oblivious=False, flag_mgn=False,
+                version="E")
+            lllr2 = LLLRv2(logits_concat, labels_concat, oblivious=False, flag_mgn=False,
+               version="F", 
+               flag_prefactor=False, 
+               classwise_sample_sizes=dummy, beta=beta,
+               flag_prior_ratio=True)
+            assert np.float16(lllr1.numpy()) == np.float16(lllr2.numpy())
+
+            lllr3 = LLLR(logits_concat, labels_concat, oblivious=True, flag_mgn=False, 
+                version="E")
+            lllr4 = LLLRv2(logits_concat, labels_concat, oblivious=True, flag_mgn=False,
+                version="F",
+                flag_prefactor=False, 
+                classwise_sample_sizes=dummy, beta=beta,
+                flag_prior_ratio=True)
+            assert np.float16(lllr3.numpy()) == np.float16(lllr4.numpy())
+    """
+    # Preprocesses
+    if flag_prior_ratio or flag_prefactor or version == "F":
+        assert classwise_sample_sizes is not None
+
+    shapes = logits_concat.shape
+    #batch_size = shapes[0]
+    order_sprt = shapes[2] - 1
+    duration = shapes[1] + order_sprt
+    num_classes = shapes[3]
+    
+    labels_oh = tf.one_hot(labels_concat, depth=num_classes, axis=1, dtype=tf.float32)
+    labels_oh = tf.reshape(labels_oh, [-1, 1, num_classes, 1])
+        # (batch, 1, num cls, 1)
+
+    # Cost weight generation 
+    if flag_prefactor or version == "F":
+        weights_allcls = cost_weight_generator(classwise_sample_sizes, beta)
+            # shape = (num classes, )
+        weights = tf.gather(weights_allcls, indices=labels_concat)
+            # shape = (batch, )
+
+    else:
+        weights_allcls = None
+        weights = 1.        
+        
+    # Calc LLRs 
+    if oblivious:
+        llrs = calc_oblivious_llrs(logits_concat, 
+            flag_prior_ratio, classwise_sample_sizes, beta) 
+    else:
+        llrs = calc_llrs(logits_concat,
+            flag_prior_ratio, classwise_sample_sizes, beta) 
+            # (batch, duration, num cls, num cls) 
+    llrs_output = llrs.numpy()
+
+    if version == "F":
+        prior = tf.reshape(np.float32(weights_allcls), [1, 1, num_classes])
+            # (1, 1, num cls, )
+        llrs -= tf.math.log(tf.expand_dims(prior, axis=3) / tf.expand_dims(prior, axis=2))
+            # (batch, duration, num cls, num cls)
+
+    # Calc LLLR
+    if flag_mgn:
+        random_margin = margin_generator(llrs, labels_oh)
+        llrs += random_margin
+            # negative for positive class row (\lambda_{k l} (k != y_i, l \in [num_classes]))
+            # posotive for negative class row (\lambda_{y_i k} (l \in [num_classes]))  
+
+    if version == "A":
+        lllr = tf.abs(labels_oh - tf.sigmoid(llrs))
+            # (batch, duration, num cls, num cls)
+        #lllr = 0.5 * (num_classes / (num_classes - 1.)) * tf.reduce_mean(lllr) 
+        if flag_prefactor:
+            lllr = tf.reduce_mean(lllr, axis=[1, 2, 3]) * weights
+                # (batch,)
+        lllr = 0.5 * (num_classes / (num_classes - 1.)) * tf.reduce_mean(lllr)
+
+    elif version == "B":
+        llrs = llrs * labels_oh
+            # (batch, duration, num cls, num cls)
+        llrs = tf.reduce_sum(llrs, axis=2)
+            # (batch, duration, num cls)
+        lllr = tf.abs(1. - tf.sigmoid(llrs))
+        #lllr = (num_classes/ (num_classes - 1)) * tf.reduce_mean(lllr) 
+        if flag_prefactor:
+            lllr = tf.reduce_mean(lllr, axis=[1, 2]) * weights
+                # (batch, )
+        lllr = (num_classes/ (num_classes - 1)) * tf.reduce_mean(lllr)
+
+    elif version == "C":
+        labels_oh = tf.tile(labels_oh, [1, duration, 1, num_classes])
+            # (batch, duration, num cls, num cls)
+        lllr = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels_oh, logits=llrs)
+            # (batch, duration, num cls, num cls)
+        #lllr = 0.5 * (num_classes/ (num_classes - 1)) * tf.reduce_mean(lllr) 
+        if flag_prefactor:
+            lllr = tf.reduce_mean(lllr, axis=[1, 2, 3]) * weights
+                # (batch,)
+        lllr = 0.5 * (num_classes/ (num_classes - 1)) * tf.reduce_mean(lllr) 
+
+    elif version == "D":
+        llrs = llrs * labels_oh
+            # (batch, duration, num cls, num cls)
+        llrs = tf.reduce_sum(llrs, axis=2)
+            # (batch, duration, num cls)
+        llrs = tf.transpose(llrs, [1, 0, 2])
+            # (duration, batch, num cls)
+        llrs = tf.reshape(llrs, [-1, num_classes])
+            # (duration * batch, num cls)
+            # A batch of llrs is repeated `duration` times.
+        z = tf.ones_like(llrs, dtype=tf.float32)
+        lllr = tf.nn.sigmoid_cross_entropy_with_logits(labels=z, logits=llrs)
+            # (duration * batch, num cls)
+        #lllr = (num_classes/ (num_classes - 1)) * tf.reduce_mean(lllr) 
+        if flag_prefactor:
+            weights = tf.tile(weights, [duration,])
+                # (duration * batch,)
+                # A batch is repeated `duration` times.
+            lllr = tf.reduce_mean(lllr, axis=[1]) * weights
+                # (duration * batch,)       
+        lllr = (num_classes/ (num_classes - 1)) * tf.reduce_mean(lllr)        
+        
+    elif version == "E" or version == "F":
+        llrs = llrs * labels_oh
+            # (batch, duration, num cls, num cls)
+        llrs = tf.reduce_sum(llrs, axis=2)
+            # (batch, duration, num cls)
+        llrs = tf.transpose(llrs, [1, 0, 2])
+            # (duration, batch, num cls)
+        llrs = tf.reshape(llrs, [-1, num_classes])
+            # (duration * batch, num cls)
+            # A batch of llrs is repeated `duration` times.
+        minllr = tf.reduce_min(llrs, axis=1, keepdims=True)
+            # (duration * batch, 1)
+        llrs = llrs - minllr 
+            # avoids over-/under-flow: 1st step
+            # (duration * batch, num cls)
+        expllrs = tf.reduce_sum(tf.math.exp(-llrs), axis=1) 
+            # (duration * batch, )
+            # Only the y_i-th rows survive.
+        lllr = - minllr + tf.math.log(expllrs + 1e-12)
+            # (duration * batch, 1)
+
+        if flag_prefactor:
+            weights = tf.tile(weights, [duration,])
+                # (duration * batch,)
+                # A batch is repeated `duration` times.
+            lllr = lllr * weights
+                # (duration * batch, 1)
+
+        lllr = tf.reduce_mean(lllr)
+            # avoids over-/under-flow: 2nd step
+                
+    else:
+        raise ValueError(
+            "version={} must be either of 'A', 'B', 'C', 'D', 'E'. or 'F'".\
+                format(version))
+
+    return lllr, llrs_output # scalar
+
 class LossDRE():
     def __init__(self, loss_type, duration, num_classes, oblivious,
         multLam=None, flag_prefactor=None, flag_prior_ratio=None,
@@ -1059,6 +1268,119 @@ def get_gradient_lstm(model, x, y, training, order_sprt, duration,
         losses = [0., multiplet_loss, lllr, wd_reg]
 
     return gradients, losses, logits_concat
+
+
+def get_gradient_lstm_ver2(model, x, y, training, order_sprt, duration, 
+    oblivious, version, flag_wd, flag_mgn, calc_grad, 
+    param_multiplet_loss, param_llr_loss, param_wd,
+    flag_prefactor=None, classwise_sample_sizes=None, beta=None,
+    flag_prior_ratio=None):
+    """Calculate loss and/or gradients.
+    Args:
+        model: A tf.keras.Model object.
+        x: A Tensor. A batch of time-series input data 
+            without sequential_slice and sequential_concat.
+        y: A Tensor. A batch of labels 
+            without sequential_slice and sequential_concat.
+        training: A boolean. Training flag.
+        order_sprt: An int. The order of the SPRT.
+        duration: An int. Num of frames in a sequence.
+        oblivious: A bool. TANDEMwO or normal TANDEM.
+        version: A string, "A", "B", "C", or "D".
+        param_multiplet_loss: A float. Loss weight.
+        param_llr_loss: A float. Loss weight.
+        param_wd: A float. Loss weight.
+        flag_wd: A boolean. Weight decay or not.
+        flag_mgn: A boolean. Use margin in LLLR or not.
+        flag_prior_ratio: A bool. Add the prior ratio term - log(p(k) / p(l)) to LLR or not.
+        flag_prefactor: A bool. Cost-sensitive or not.
+        beta: A float larger than 0. Larger beta leads to 
+            more discriminative weights.
+            If beta is None, normal, non-cost-sensitive learning
+            will be done. If beta = 1, then weights are simply 
+            the inverse class frequencies (1 / N_k, 
+            where N_k is the sample size of class k).
+            If beta = -1, weights = [1,1,...,1] (len=num classes).
+            This is useful for non-cost-sensitive learning.
+        classwise_sample_sizes: A list of integers. 
+            The length is equal to the number of classes.
+    Returns:
+        gradients: A Tensor or None.
+        losses: A list of loss Tensors; namely,
+            total_loss: A scalar Tensor or 0 if not calc_grad. 
+                The weighted total loss.
+            multiplet_loss: A scalar Tensor.
+            llr_loss: A scalar Tensor.
+            wd_reg: A scalar Tensor.
+        logits_concat: A logit Tensor with shape 
+            (batch, (duration - order_sprt), order_sprt + 1, num classes). 
+            This is the output of 
+            datasets.data_processing.sequential_concat(logit_slice, labels_slice).
+    Remarks:
+        - All the losses below will be calculated if not calc_grad 
+          to log them to TensorBoard.
+            total_loss 
+            multiplet_loss
+            llr_loss 
+    """
+    x_slice, y_slice = sequential_slice(x, y, order_sprt)
+
+    # For training
+    if calc_grad:
+        with tf.GradientTape() as tape:
+            logits_slice = model(x_slice, training)
+            logits_concat, y_concat = sequential_concat(
+                logits_slice, y_slice, duration)
+            total_loss = 0.
+
+            # multiplet_loss, llr_loss
+            if param_multiplet_loss != 0:
+                multiplet_loss = multiplet_loss_func(logits_slice, y_slice,
+                    flag_prefactor, classwise_sample_sizes, beta)
+                total_loss += param_multiplet_loss * multiplet_loss
+            else:
+                multiplet_loss = 0.
+
+            if param_llr_loss != 0:
+                lllr, LLRs = LLLR_ver2(logits_concat, y_concat, oblivious, version, flag_mgn,
+                        flag_prefactor, flag_prior_ratio, classwise_sample_sizes, beta)
+                
+                total_loss += param_llr_loss * lllr
+            else:
+                lllr = 0.
+
+            # wd_reg
+            wd_reg = 0.
+            if flag_wd:
+                for variables in model.trainable_variables:
+                    wd_reg += tf.nn.l2_loss(variables)
+                    total_loss += param_wd * wd_reg
+
+        gradients = tape.gradient(total_loss, model.trainable_variables)
+        losses = [total_loss, multiplet_loss, lllr, wd_reg]
+
+    # For validation and test
+    else: 
+        logits_slice = model(x_slice, training)
+        logits_concat, y_concat = sequential_concat(
+            logits_slice, y_slice, duration)
+        total_loss = 0.
+
+        # multiplet_loss, llr_loss
+        multiplet_loss = multiplet_loss_func(logits_slice, y_slice,
+                    flag_prefactor, classwise_sample_sizes, beta)
+        lllr, LLRs = LLLR_ver2(logits_concat, y_concat, oblivious, version, flag_mgn,
+            flag_prefactor, flag_prior_ratio, classwise_sample_sizes, beta)
+
+        # wd_reg
+        wd_reg = 0.
+        # for variables in model.trainable_variables:
+        #     wd_reg += tf.nn.l2_loss(variables)
+
+        gradients = None
+        losses = [0., multiplet_loss, lllr, wd_reg]
+
+    return gradients, losses, logits_concat, LLRs
 
 
 def get_loss_fe(model, x, y, flag_wd, training, calc_grad, param_wd):
